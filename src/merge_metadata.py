a2ff01bee5921ac616fc42dea83fe405464478d0
Greegko <greegko+github@gmail.com>
2023-01-26 20:41:29 +0100

    arg parser

--- src/merge_metadata.py
+++ src/merge_metadata.py
@@ -1,18 +1,25 @@
 import os
 import sys
 from process_folder import processFolder
+import argparse
 
-try: 
-  path = sys.argv[1]
+parser = argparse.ArgumentParser()
 
-  if not os.path.exists(path):
-    print('Target folder doesnt exist')
-    exit()
-except:
-    print('Provide target folder!')
-    exit()
+parser.add_argument('source_folder')
+parser.add_argument('output_folder', nargs='?')
+parser.add_argument('-w',  '--edited_word', default='edited')
+parser.add_argument('-rm', '--remove_metadata', action='store_true')
+parser.add_argument('-e',  '--edit_inline', action='store_true')
+parser.add_argument('-o',  '--optimize', type=int, default=75)
 
-out_folder = len(sys.argv) > 2 and sys.argv[2]
-replace = bool(out_folder)
+args = parser.parse_args()
 
-processFolder(path, 'edited', 75, replace, out_folder)
+if not os.path.exists(args.source_folder):
+  print('Target folder doesn\'t exist')
+  exit()
+
+if args.edit_inline and args.output_folder:
+  print('Cannot edit inline and having an output folder')
+  exit()
+
+processFolder(args.source_folder, args.edited_word, args.optimize, args.remove_metadata, args.edit_inline, args.output_folder)
